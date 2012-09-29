834e18873e1be7c1fac449119f3ae4c361bbf86b
Ros Dostup <admin@rosdostup.ru>
2012-09-30 01:03:05 +0400

    добавил ссылки на Вики в страницах

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -2,4 +2,4 @@
 $this->pageTitle=Yii::app()->name . ' :: О проекте';
 ?>
 <h1>О проекте</h1>
-<iframe height="600" width="990" frameBorder="3" src="http://rosdostup.ru/dokuwiki_integrated/doc.php?id=about:about_project" onload="$(this).height($(this).contents().height()); $(this).width($(this).contents().width());">your browser does not support IFRAMEs</iframe>
+http://rosdostup.ru/dokuwiki_integrated/doc.php?id=about:about_project
--- protected/views/site/pages/regulations.php
+++ protected/views/site/pages/regulations.php
@@ -3,4 +3,4 @@ $this->pageTitle=Yii::app()->name . ' - Стандарты и нормативы
 $this->title='Стандарты и нормативы';
 $this->layout='//layouts/header_blank';
 ?>
-<iframe height="600" width="990" frameBorder="3" onload="$(this).height($(this).contents().height()); $(this).width($(this).contents().width());" src="http://rosdostup.ru/dokuwiki_integrated/doc.php?id=regulations">your browser does not support IFRAMEs</iframe>
+http://rosdostup.ru/dokuwiki_integrated/doc.php?id=regulations
