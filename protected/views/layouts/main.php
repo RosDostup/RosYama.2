796ed5c0d31b4eaaaf5b9f32f2a4b49a2ea782e2
Ahaenor <ahaenor@gmail.com>
2012-10-01 14:13:06 +0400

    Из главного меню сайта убран пункт "Справочники".

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -41,7 +41,7 @@
 				array('label'=>'Нормативы', 'url'=>array('/site/page', 'view'=>'regulations')),
 				array('label'=>'Статистика', 'url'=>array('/statics/index')),
 				array('label'=>'FAQ', 'url'=>array('/site/page', 'view'=>'faq')),
-				array('label'=>'Справочники', 'url'=>array('/sprav/index')),
+//				array('label'=>'Справочники', 'url'=>array('/sprav/index')), // Намеренно убрано для Росдоступа
 				//array('label'=>'Logout ('.$this->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!$this->user->isGuest)
 			),
 			'htmlOptions'=>array('class'=>'menu'),
