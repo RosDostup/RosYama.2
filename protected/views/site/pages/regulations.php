f0e71be58c4f8414d8b7f9f6495a93bb8824f9cb
kulandin <admin@rosdostup.ru>
2012-10-12 15:00:32 +0400

    вернул значки дефектов в st1234, добавил картинки 'о РосДоступе', убрал мелкую ошибку в тексте заявления

--- protected/views/site/pages/regulations.php
+++ protected/views/site/pages/regulations.php
@@ -3,4 +3,4 @@ $this->pageTitle=Yii::app()->name . ' - Стандарты и нормативы
 $this->title='Стандарты и нормативы';
 $this->layout='//layouts/header_blank';
 ?>
-<iframe height="600" width="990" frameBorder="3" src="http://rosdostup.ru/dokuwiki_integrated/doc.php?id=regulations">your browser does not support IFRAMEs</iframe>
+<iframe height="1100" width="990" frameBorder="3" src="http://rosdostup.ru/dokuwiki_integrated/doc.php?id=regulations">your browser does not support IFRAMEs</iframe>
